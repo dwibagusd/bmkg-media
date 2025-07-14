adf1843387fadb3938f7f5934dca2592bc1b3de2
dwibagusd <115286631+dwibagusd@users.noreply.github.com>
2025-07-14 17:11:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -548,8 +548,9 @@ def logout():
 
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
     # migrate_data()
     with app.app_context():
         init_db()
