8dc12e8681d8b9cb06fb15bd426f33739f2fbb4b
dwibagusd <115286631+dwibagusd@users.noreply.github.com>
2025-11-14 16:16:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ from io import BytesIO
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart   
-import tempfile                          
+import tempfile      
+import json
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
@@ -1057,6 +1058,7 @@ with app.app_context():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
